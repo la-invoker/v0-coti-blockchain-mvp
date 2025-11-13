829a9c27bacd5abdb9b841df21fd01716210b65a
jordan <jordanlaphon@gmail.com>
2025-11-13 17:56:25 +0700

    update

--- lib/coti/config.ts
+++ lib/coti/config.ts
@@ -6,91 +6,233 @@ export const COTI_CONFIG = {
 }
 
 export const CONTRACT_ABI = [
-  // Events
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: true, internalType: "uint64", name: "id", type: "uint64" },
-      { indexed: false, internalType: "uint64", name: "timestamp", type: "uint64" },
-      { indexed: true, internalType: "address", name: "addedBy", type: "address" },
-    ],
-    name: "DataSubmitted",
-    type: "event",
-  },
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: true, internalType: "uint64", name: "id", type: "uint64" },
-      { indexed: true, internalType: "address", name: "deletedBy", type: "address" },
-    ],
-    name: "DataDeleted",
-    type: "event",
-  },
-  // Functions
-  {
-    inputs: [
-      { internalType: "itString", name: "encryptedJson", type: "itString" },
-      { internalType: "itUint64", name: "encryptedTimestamp", type: "itUint64" },
-    ],
-    name: "submitData",
-    outputs: [{ internalType: "uint64", name: "", type: "uint64" }],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [{ internalType: "uint64", name: "id", type: "uint64" }],
-    name: "getData",
-    outputs: [
-      { internalType: "ctString", name: "encryptedJson", type: "ctString" },
-      { internalType: "ctUint64", name: "encryptedTimestamp", type: "ctUint64" },
-      { internalType: "bool", name: "isActive", type: "bool" },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      { internalType: "uint256", name: "offset", type: "uint256" },
-      { internalType: "uint256", name: "limit", type: "uint256" },
-    ],
-    name: "getDataIds",
-    outputs: [{ internalType: "uint64[]", name: "", type: "uint64[]" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "getTotalCount",
-    outputs: [{ internalType: "uint64", name: "", type: "uint64" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "getActiveCount",
-    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [{ internalType: "uint64", name: "id", type: "uint64" }],
-    name: "deleteData",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "owner",
-    outputs: [{ internalType: "address", name: "", type: "address" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [{ internalType: "address", name: "newOwner", type: "address" }],
-    name: "transferOwnership",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-] as const
+    {
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "uint64",
+          "name": "id",
+          "type": "uint64"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "deletedBy",
+          "type": "address"
+        }
+      ],
+      "name": "DataDeleted",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "uint64",
+          "name": "id",
+          "type": "uint64"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint64",
+          "name": "timestamp",
+          "type": "uint64"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "addedBy",
+          "type": "address"
+        }
+      ],
+      "name": "DataSubmitted",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint64",
+          "name": "id",
+          "type": "uint64"
+        }
+      ],
+      "name": "deleteData",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getActiveCount",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint64",
+          "name": "id",
+          "type": "uint64"
+        }
+      ],
+      "name": "getData",
+      "outputs": [
+        {
+          "components": [
+            {
+              "internalType": "ctUint64[]",
+              "name": "value",
+              "type": "uint256[]"
+            }
+          ],
+          "internalType": "struct ctString",
+          "name": "encryptedJson",
+          "type": "tuple"
+        },
+        {
+          "internalType": "ctUint64",
+          "name": "encryptedTimestamp",
+          "type": "uint256"
+        },
+        {
+          "internalType": "bool",
+          "name": "isActive",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "offset",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "limit",
+          "type": "uint256"
+        }
+      ],
+      "name": "getDataIds",
+      "outputs": [
+        {
+          "internalType": "uint64[]",
+          "name": "",
+          "type": "uint64[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getTotalCount",
+      "outputs": [
+        {
+          "internalType": "uint64",
+          "name": "",
+          "type": "uint64"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "owner",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "components": [
+            {
+              "components": [
+                {
+                  "internalType": "ctUint64[]",
+                  "name": "value",
+                  "type": "uint256[]"
+                }
+              ],
+              "internalType": "struct ctString",
+              "name": "ciphertext",
+              "type": "tuple"
+            },
+            {
+              "internalType": "bytes[]",
+              "name": "signature",
+              "type": "bytes[]"
+            }
+          ],
+          "internalType": "struct itString",
+          "name": "encryptedJson",
+          "type": "tuple"
+        },
+        {
+          "components": [
+            {
+              "internalType": "ctUint64",
+              "name": "ciphertext",
+              "type": "uint256"
+            },
+            {
+              "internalType": "bytes",
+              "name": "signature",
+              "type": "bytes"
+            }
+          ],
+          "internalType": "struct itUint64",
+          "name": "encryptedTimestamp",
+          "type": "tuple"
+        }
+      ],
+      "name": "submitData",
+      "outputs": [
+        {
+          "internalType": "uint64",
+          "name": "",
+          "type": "uint64"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "transferOwnership",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ]as const
